94d0479840ce8852c6ae0ebde1a192582edcdd57
Shibu Narayanan <shibunarayan@gmail.compwd>
2025-09-01 20:45:32 +0530

    Common  modules for odoo repos

--- g2p_auth_oidc/__manifest__.py
+++ g2p_auth_oidc/__manifest__.py
@@ -1,10 +1,9 @@
 {
     "name": "OpenID Connect Authentication",
-    "version": "17.0.1.0.0",
+    "version": "17.0.0.0.0",
     "author": "OpenG2P",
     "website": "https://openg2p.org",
-    "license": "Other OSI approved licence",
-    "development_status": "Alpha",
+    "license": "LGPL-3",
     "external_dependencies": {"python": ["python-jose"]},
     "depends": ["auth_oauth"],
     "data": [
