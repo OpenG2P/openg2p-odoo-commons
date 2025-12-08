96955699a6df8a025825419bd5b7636f390cc407
EyuaelB <eyuaelbezabeh@gmail.com>
2025-12-08 14:14:17 +0300

    change log Added

--- g2p_change_log/__manifest__.py
+++ g2p_change_log/__manifest__.py
@@ -1,23 +1,22 @@
 {
-    "name": "OpenG2P Change Log",
-    "category": "G2P",
-    "version": "17.0.1.0.0",
-    "sequence": 1,
+    "name": "G2P Change Log",
+    "version": "17.0.1.1.0",
     "author": "OpenG2P",
-    "website": "https://openg2p.org",
-    "license": "Other OSI approved licence",
-    "development_status": "Alpha",
-    # any module necessary for this one to work correctly
+    "license": "",
+    "website": "https://github.com/OpenG2P/openg2p-odoo-commons/g2p_change_log",
+    "category": "Tools",
     "depends": ["base"],
-    # always loaded
     "data": [
-        # 'security/ir.model.access.csv',
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "data/audit_db_config.xml",
+        "views/g2p_change_rule_view.xml",
+        "views/g2p_change_log_view.xml",
+        "views/g2p_change_log_line_view.xml",
     ],
-    "assets": {},
-    "external_dependencies": {},
-    "demo": [],
-    "images": [],
+    "external_dependencies": {
+        "python": ["psycopg2"],
+    },
     "application": True,
     "installable": True,
-    "auto_install": False,
 }
